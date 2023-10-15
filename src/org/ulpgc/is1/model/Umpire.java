ffeae6ae55988b07414a9166555229475e2512f1
Margarita <cruzperez.marga@gmail.com>
2023-10-15 16:53:19 +0100

    Hemos añadido la clase Umpire, con sus atributos necesarios y el getter getName.

---
+++ src/org/ulpgc/is1/model/Umpire.java
@@ -0,0 +1,14 @@
+package org.ulpgc.is1.model;
+
+public class Umpire {
+    public String name();
+    public String surname();
+
+    public Umpire(String name, String surname) {
+        this.name() = name;
+        this.surname() = surname;
+    }
+    public String getName() {
+        return this.name();
+    }
+}
