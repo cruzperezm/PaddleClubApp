53f867d05685227398f9e43b5ee7a1a1ebf67526
Margarita <cruzperez.marga@gmail.com>
2023-10-15 16:42:37 +0100

    Hemos añadido la clase Adress, con sus atributos necesarios.

---
+++ src/org/ulpgc/is1/model/Adress.java
@@ -0,0 +1,15 @@
+package org.ulpgc.is1.model;
+
+public class Adress  {
+    public String street();
+    public int number();
+    public int postalCode();
+    public String city();
+
+    public Adress(String street, int number, int postalCode, String city) {
+        this.street() = street;
+        this.number() = number;
+        this.postalCode() = postalCode;
+        this.city() = city;
+    }
+}
