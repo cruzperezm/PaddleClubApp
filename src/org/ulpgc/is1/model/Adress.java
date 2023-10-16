e0b35fbc5110919f4c33bc493cc25287ba9f8b5d
Margarita <cruzperez.marga@gmail.com>
2023-10-16 10:10:15 +0100

    Hemos añadido la clase Customer, con sus atributos necesarios, y sus getter y setter. Sin embargo, al no haber añadido el enumerador CourtType, éste ha dado problemas

--- src/org/ulpgc/is1/model/Adress.java
+++ src/org/ulpgc/is1/model/Adress.java
@@ -1,10 +1,10 @@
 package org.ulpgc.is1.model;
 
 public class Adress  {
-    public String street();
-    public int number();
-    public int postalCode();
-    public String city();
+    public String street;
+    public int number;
+    public int postalCode;
+    public String city;
 
     public Adress(String street, int number, int postalCode, String city) {
         this.street() = street;
--- src/org/ulpgc/is1/model/Umpire.java
+++ src/org/ulpgc/is1/model/Umpire.java
@@ -1,8 +1,8 @@
 package org.ulpgc.is1.model;
 
 public class Umpire {
-    public String name();
-    public String surname();
+    public String name;
+    public String surname;
 
     public Umpire(String name, String surname) {
         this.name() = name;
