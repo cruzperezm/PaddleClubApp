d69f3b055cbdc5c7d30e869b93379f5960a0d633
Margarita <cruzperez.marga@gmail.com>
2023-10-16 12:01:36 +0100

    Hemos añadido la clase Court, junto a sus atributos.

---
+++ src/org/ulpgc/is1/model/court.java
@@ -0,0 +1,37 @@
+package org.ulpgc.is1.model;
+
+public class Court {
+    public String name;
+    public int price;
+    public CourtType type;
+
+    public Court(String name, int price, CourtType type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public CourtType getType() {
+        return type;
+    }
+
+    public void setType(CourtType type) {
+        this.type = type;
+    }
+}
