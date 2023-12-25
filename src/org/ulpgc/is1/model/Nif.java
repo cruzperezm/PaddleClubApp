b08470b326b5234021040be5f97ccd3d0ff452c7
cruzperezm <cruzperez.marga@gmail.com>
2023-12-25 16:02:40 +0000

    Corrección del método isValid en la clase NIF.

--- src/org/ulpgc/is1/model/Nif.java
+++ src/org/ulpgc/is1/model/Nif.java
@@ -1,4 +1,6 @@
 package org.ulpgc.is1.model;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Nif {
 
@@ -7,20 +9,19 @@ public class Nif {
     public Nif(String number){
         this.number = number;
     }
-    public boolean isValid(){
-        String dni = this.number;
-        if (dni.length() != 9){
-            return false;
-        }
-        // String number = dni.substring(0, 9);
-        // String letra = dni.substring(9);
-        return true;
-    }
+
+    private boolean isValid(String input) {
+        String pattern = "\\d{8}[a-zA-Z]";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(input);
+
+        return matcher.matches();}
+
     public String getNumber() {
-        if(!isValid()){
+        if(!isValid(this.number)){
             return "XXXX";
         }
-        return number;
+        return this.number;
     }
     public void setNumber(String number) {
         this.number = number;
